ffea06823f99cb76c5c2c9a94a33f2d8dbe5a111
Nathan CRUAUD <nathan.cruaud@grenoble-inp.org>
2021-03-12 16:54:17 +0100

    Creation client

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,9 +1,19 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import metier.Client;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> listeJeu = new ArrayList();
+		listeJeu.add("Pokemon");
+		listeJeu.add("Mario");
+		listeJeu.add("Dofus");
+		
+		Client client = new Client("Dupont", "Jean", listeJeu);
 
 	}
 
